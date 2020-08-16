a675a22938f0f1a07539c42d477dcedb580c545a
mayank-genesis <mayank16meena@gmail.com>
2020-08-16 18:06:40 +0530

    Updates

---
+++ precache-manifest.6ee452220c9e26c04f4d46769e997e46.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4fb588e3d14d9e39057f6b79238d8e",
+    "url": "/trix/index.html"
+  },
+  {
+    "revision": "235551f03587793f36cc",
+    "url": "/trix/static/css/main.42c188cc.chunk.css"
+  },
+  {
+    "revision": "26df0da2752b986e8b15",
+    "url": "/trix/static/js/2.d1ea5ba6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/trix/static/js/2.d1ea5ba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "235551f03587793f36cc",
+    "url": "/trix/static/js/main.9b04c385.chunk.js"
+  },
+  {
+    "revision": "bd4690473032a4f4c410",
+    "url": "/trix/static/js/runtime-main.12bebf87.js"
+  },
+  {
+    "revision": "cfe6d7aa5f7a1a36c4b99d2f8de4540c",
+    "url": "/trix/static/media/1.cfe6d7aa.jpg"
+  },
+  {
+    "revision": "5a9521806e15d6147576354613baf10a",
+    "url": "/trix/static/media/2.5a952180.jpg"
+  },
+  {
+    "revision": "98d44651478a41757d3db0c363fc25ae",
+    "url": "/trix/static/media/am.98d44651.jpg"
+  },
+  {
+    "revision": "45e281b41682a66a54e5e020599d0729",
+    "url": "/trix/static/media/av.45e281b4.jpg"
+  },
+  {
+    "revision": "949d9a29b176eb3ce2ca4df4f464b43b",
+    "url": "/trix/static/media/bw.949d9a29.jpg"
+  },
+  {
+    "revision": "14ca93eea4e0277d3e81fda063784745",
+    "url": "/trix/static/media/hb.14ca93ee.jpg"
+  },
+  {
+    "revision": "09766d06badce031f67ecf5d9d7be13a",
+    "url": "/trix/static/media/im.09766d06.png"
+  },
+  {
+    "revision": "eb3c880274f70fa435df63527945a9e0",
+    "url": "/trix/static/media/im2.eb3c8802.jpg"
+  },
+  {
+    "revision": "dd46b1add8103ce498ea447ffa629fc7",
+    "url": "/trix/static/media/iw.dd46b1ad.jpg"
+  },
+  {
+    "revision": "801bb60b7e3cf193e826798cfa58b76e",
+    "url": "/trix/static/media/jl.801bb60b.jpg"
+  },
+  {
+    "revision": "75651c7937ec167f6a64e5c1de4fc4a4",
+    "url": "/trix/static/media/joker.75651c79.jpg"
+  },
+  {
+    "revision": "2cfcceff2045105e751f6d64506e0112",
+    "url": "/trix/static/media/joker2.2cfcceff.jfif"
+  },
+  {
+    "revision": "34a4f31c87f0e3f9e7c682ebf7eb3664",
+    "url": "/trix/static/media/left-arrow.34a4f31c.svg"
+  },
+  {
+    "revision": "dc02507f04ea09daa9cf2208085de997",
+    "url": "/trix/static/media/left-arrow.dc02507f.png"
+  },
+  {
+    "revision": "5e5b72cc72ffd2c9ea04f8a4742dd6b1",
+    "url": "/trix/static/media/right-arrow.5e5b72cc.svg"
+  },
+  {
+    "revision": "6bc3875215918cd3a2b0d6564941954f",
+    "url": "/trix/static/media/sr.6bc38752.jpg"
+  },
+  {
+    "revision": "4496c1c7fc04fcbd13a140139b5b382b",
+    "url": "/trix/static/media/venom.4496c1c7.jpg"
+  },
+  {
+    "revision": "cf51b3e041846bd10c52109d4f5798d6",
+    "url": "/trix/static/media/ww.cf51b3e0.jpg"
+  }
+]);
